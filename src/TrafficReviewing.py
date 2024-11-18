e0f3d37cf2a696012430f56fe7d5f32d83c6f69c
float321 <187214682+float321@users.noreply.github.com>
2024-11-18 18:19:45 +0300

    Update TrafficReviewing.py

--- src/TrafficReviewing.py
+++ src/TrafficReviewing.py
@@ -3,142 +3,9 @@ import matplotlib.pyplot as plt
 import csv
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog, QLabel, QTableWidgetItem, QTableWidget
 from PyQt6.QtWidgets import QInputDialog, QLineEdit
-from PyQt6.QtGui import QColor
-
-
-class MiniCalculator(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setGeometry(200, 200, 500, 300)
-        self.setWindowTitle('MiniCalculator')
-
-        self.first_num = QLabel('Первое число:', self)
-        self.second_num = QLabel('Второе число:', self)
-        self.lbl3 = QLabel('Подсчитать целое выражение:', self)
-
-        self.number_1 = QLineEdit(self)
-        self.number_2 = QLineEdit(self)
-        self.expression = QLineEdit(self)
-
-        self.calculate_button = QPushButton('->', self)
-        self.expression_button = QPushButton('=', self)
-
-        self.lblsum = QLabel('Сумма:', self)
-        self.lblsum.resize(self.lblsum.sizeHint())
-        self.lblsum.move(200, 10)
-        self.lblrazn = QLabel('Разность:', self)
-        self.lblrazn.resize(self.lblrazn.sizeHint())
-        self.lblrazn.move(200, 30)
-        self.lblymn = QLabel('Произведение:', self)
-        self.lblymn.resize(self.lblymn.sizeHint())
-        self.lblymn.move(200, 50)
-        self.lbldel = QLabel('Частное:', self)
-        self.lbldel.resize(self.lbldel.sizeHint())
-        self.lbldel.move(200, 70)
-
-        self.result_sum = QLineEdit(self)
-        self.result_sum.resize(50, 20)
-        self.result_sum.move(300, 10)
-        self.result_sub = QLineEdit(self)
-        self.result_sub.resize(50, 20)
-        self.result_sub.move(300, 30)
-        self.result_mul = QLineEdit(self)
-        self.result_mul.resize(50, 20)
-        self.result_mul.move(300, 50)
-        self.result_div = QLineEdit(self)
-        self.result_div.resize(50, 20)
-        self.result_div.move(300, 70)
-        self.result_expression = QLineEdit(self)
-        self.result_expression.resize(50, 20)
-        self.result_expression.move(160, 150)
-
-        self.first_num.resize(self.first_num.sizeHint())
-        self.number_1.resize(50, 30)
-        self.first_num.move(10, 10)
-        self.number_1.move(10, 30)
-
-        self.second_num.resize(self.second_num.sizeHint())
-        self.number_2.resize(50, 30)
-        self.second_num.move(10, 70)
-        self.number_2.move(10, 90)
-
-        self.lbl3.resize(self.lbl3.sizeHint())
-        self.lbl3.move(10, 120)
-        self.expression.resize(100, 20)
-        self.expression.move(10, 150)
-
-        self.calculate_button.resize(30, 20)
-        self.calculate_button.move(150, 50)
-        self.expression_button.resize(30, 20)
-        self.expression_button.move(120, 150)
-
-        self.calculate_button.clicked.connect(self.click)
-        self.expression_button.clicked.connect(self.expression_making)
-
-    def click(self):
-        a = self.number_1.text()
-        b = self.number_2.text()
-        if a.isdigit() and b.isdigit():
-            a = int(a)
-            b = int(b)
-            self.result_sum.setText(str(a + b))
-            self.result_sub.setText(str(a - b))
-            self.result_mul.setText(str(a * b))
-            if b != 0:
-                self.result_div.setText(str(round(a / b, 3)))
-            else:
-                self.result_div.setText('Error')
-
-    def expression_making(self):
-        try:
-            self.result_expression.setText(str(eval(self.expression.text())))
-        except Exception:
-            self.result_expression.setText('Error')
-
-
-class NewTabl(QMainWindow):
-    def __init__(self, a):
-        super().__init__()
-        self.setGeometry(600, 600, 500, 500)
-        self.setWindowTitle('Table Review')
-
-        t = 0
-        for i in a[1:]:
-            t += int(i[2])
-        t //= len(a[1:])
-
-        a1 = sorted(a[1:], key=lambda x: int(x[2]))
-
-        self.tabll = QTableWidget(self)
-        self.tabll.setColumnCount(len(a[0]))
-        self.tabll.setHorizontalHeaderLabels(a[0])
-        self.tabll.setRowCount(0)
-        for i, row in enumerate(a1):
-            self.tabll.setRowCount(
-                self.tabll.rowCount() + 1)
-            for j, elem in enumerate(row):
-                self.tabll.setItem(
-                    i, j, QTableWidgetItem(elem))
-            if int(row[2]) < t / 3:
-                self.good_num(i)
-            elif int(row[2]) > t * 2 / 3:
-                self.bad_num(i)
-            else:
-                self.sredn_num(i)
-
-        self.tabll.resize(500, 500)
-
-    def bad_num(self, row):
-        for i in range(self.tabll.columnCount()):
-            self.tabll.item(row, i).setBackground(QColor(200, 0, 0))
-
-    def sredn_num(self, row):
-        for i in range(self.tabll.columnCount()):
-            self.tabll.item(row, i).setBackground(QColor(100, 100, 0))
-
-    def good_num(self, row):
-        for i in range(self.tabll.columnCount()):
-            self.tabll.item(row, i).setBackground(QColor(0, 200, 0))
+from Mapopener import Mapping
+from Calculatoropener import MiniCalculator
+from Tableopener import NewTabl
 
 
 class Traffic(QMainWindow):
@@ -230,6 +97,11 @@ class Traffic(QMainWindow):
         self.btn2.resize(self.btn2.sizeHint())
         self.btn2.move(300, 460)
         self.btn2.hide()
+
+        self.btnmap = QPushButton('Открыть карту', self)
+        self.btnmap.clicked.connect(self.openmap)
+        self.btnmap.resize(self.btnmap.sizeHint())
+        self.btnmap.move(250, 50)
 
     def choose(self):
         self.dialog = QFileDialog.getOpenFileName(self, 'Выбрать таблицу', '', 'Таблица (*.csv)')[0]
@@ -394,6 +266,10 @@ class Traffic(QMainWindow):
     def calcopenning(self):
         self.calc = MiniCalculator()
         self.calc.show()
+
+    def openmap(self):
+        self.map = Mapping()
+        self.map.show()
 
 
 if __name__ == '__main__':
